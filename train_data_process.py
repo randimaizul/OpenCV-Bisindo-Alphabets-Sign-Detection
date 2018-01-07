553d6858b568df07ebe5fbe1d0dd70e2ee0dd77a
DESKTOP-OVM0E95\Randi <rmaizul@gmail.com>
2018-01-07 13:21:47 +0700

    morfologi

--- train_data_process.py
+++ train_data_process.py
@@ -4,17 +4,19 @@ from header import image_enhanchment
 from matplotlib import pyplot as plt
 
 
-img = cv2.imread("train_data/rya/N/1.png")
+img = cv2.imread("train_data/1.png")
 cv2.imshow('origin',img)
 
 ret2,thresh = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
 cv2.imshow("threshold",thresh)
 gray_image = cv2.cvtColor(thresh, cv2.COLOR_BGR2GRAY)
 
+cv2.imshow("threshold2",gray_image)
+
 th2 = cv2.adaptiveThreshold(gray_image,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)
 th3 = cv2.adaptiveThreshold(gray_image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-cv2.imshow("th2",th2)
-cv2.imshow("th3",th3)
+cv2.imshow("th2_ADAPTIVE_THRESH_MEAN_C",th2)
+cv2.imshow("th3_ADAPTIVE_THRESH_GAUSSIAN_C",th3)
 
 filename = "train_data/Nth2.png"
 filename1 = "train_data/Nth3.png"
